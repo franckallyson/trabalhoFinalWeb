5347589a32aed7584257b27de24518f2ac464020
franc <franckallyson@hotmail.com>
2023-04-10 15:11:52 -0300

    correção settings

--- api/backend_trabalho_final/settings.py
+++ api/backend_trabalho_final/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-*teipvlyfox9pu%z_d6tsexd-&64asglzzr*hijhpqnj#wc4&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
